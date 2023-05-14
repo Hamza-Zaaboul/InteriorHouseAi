23595657e6d34147a34425e1566e272809e27370
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-05-14 17:01:38 +0200

    ere

--- components/galerie/GaleryStyle.jsx
+++ components/galerie/GaleryStyle.jsx
@@ -13,54 +13,54 @@
   ```
 */
 const files = [
-    {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
-      {
-        title: "IMG_4985.HEIC",
-        size:"Outside - park",
-        source:
-          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
-      },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  {
+    title: "IMG_4985.HEIC",
+    size: "Outside - park",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
   // More files...
 ];
 
@@ -73,7 +73,7 @@ export default function GaleryStyle() {
             Apparences
           </h2>
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl mt-2 ">
-          Décors et Styles vestimentaires
+            Décors et Styles vestimentaires
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-100">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -83,22 +83,23 @@ export default function GaleryStyle() {
         </div>
         <ul
           role="list"
-          className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+          className="grid grid-cols-2 gap-4 mt-8 sm:mt-12 lg:grid-cols-4 sm:gap-5 lg:mt-16"
         >
+            
           {files.map((file) => (
             <li key={file.source} className="relative">
               <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                <img
-                  src={file.source}
-                  alt=""
-                  className="pointer-events-none object-cover group-hover:opacity-75"
-                />
-                <button
-                  type="button"
-                  className="absolute inset-0 focus:outline-none"
-                >
-                  <span className="sr-only">View details for {file.title}</span>
-                </button>
+                <div className="flex w-full">
+                  <img
+                    src={file.source}
+                    alt=""
+                    className="object-cover w-1/2 h-full"                  />
+                  <img
+                    src={file.source}
+                    alt=""
+                    className="object-cover w-1/2 h-full"
+                  />
+                </div>
               </div>
               <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
                 {file.title}
@@ -108,6 +109,7 @@ export default function GaleryStyle() {
               </p>
             </li>
           ))}
+          
         </ul>
       </div>
     </div>
