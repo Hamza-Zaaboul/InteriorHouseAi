18b66aec1a8bd48f58478caa50b3bee7c4d7b4c9
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-02 00:12:35 +0200

    Modale ajustement

--- components/dashboardui/utils/DeletedButton.jsx
+++ components/dashboardui/utils/DeletedButton.jsx
@@ -2,44 +2,139 @@ import { deleteDocument } from "@/firebase/Firestore/deletData";
 import { deleteFilesByURLs } from "@/firebase/Storage/delete";
 import { useAuthContext } from "@/store/AuthContext";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
-export default function DeletedButton({urlselected, listselected, actualiser }) {
+export default function DeletedButton({
+  urlselected,
+  listselected,
+  actualiser,
+}) {
   const { user } = useAuthContext();
+  const [open, setOpen] = useState(false);
 
-  const handleButtonClick = async () => {
+  const cancelButtonRef = useRef(null);
+
+  const handleDeleteClick = async () => {
+    setOpen(false); // Fermer le modal
+
     try {
       try {
-      await deleteFilesByURLs(urlselected);
-      }
-      catch (error) {
+        await deleteFilesByURLs(urlselected);
+      } catch (error) {
         console.error(error);
       }
-
 
       for (const itemId of listselected) {
         console.log(`Tentative de suppression de l'élément ${itemId}.`);
         await deleteDocument("Archives", user.uid, "Historique", itemId);
-        actualiser();
       }
+
+      actualiser();
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleButtonClick = () => {
+    setOpen(true); // Ouvrir le modal
+  };
+
   return (
-    <div className="w-full flex items-center justify-center relative h-full py-2">
-      <span className="ml-3 block">
-        <button
-          onClick={handleButtonClick}
-          className="inline-flex items-center rounded-md bg-gray-400 px-3 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500"
+    <>
+      <div className="w-full flex items-center justify-center relative h-full py-2">
+        <span className="ml-3 block">
+          <button
+            onClick={handleButtonClick}
+            className="inline-flex items-center rounded-md bg-gray-400 px-3 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500"
+          >
+            <TrashIcon
+              className="-ml-0.5 mr-1.5 h-5 w-5 text-white"
+              aria-hidden="true"
+            />
+            Supprimer
+          </button>
+        </span>
+      </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
         >
-          <TrashIcon
-            className="-ml-0.5 mr-1.5 h-5 w-5 text-white"
-            aria-hidden="true"
-          />
-          Supprimer
-        </button>
-      </span>
-    </div>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationTriangleIcon
+                          className="h-6 w-6 text-red-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Supprimer les éléments sélectionnés
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Êtes-vous sûr de vouloir supprimer les éléments
+                            sélectionnés ? Cette action est irréversible.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      onClick={handleDeleteClick}
+                    >
+                      Supprimer
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Annuler
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 }
