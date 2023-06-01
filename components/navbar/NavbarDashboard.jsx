96293677ea1d35ee2e0d8df4281e8d36bc05e81b
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-02 00:58:23 +0200

    loading desabled

--- components/navbar/NavbarDashboard.jsx
+++ components/navbar/NavbarDashboard.jsx
@@ -3,7 +3,7 @@ import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { signOutUser } from "@/firebase/Auth/logout";
-import firebase_app from "@/firebase/InitFirebase";
+
 import { useAuthContext } from "@/store/AuthContext";
 
 const navigation = [
