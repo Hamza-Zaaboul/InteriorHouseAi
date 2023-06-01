6efeb5d0930e61556c1302487c94a6898cf8f4c6
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-01 15:09:27 +0200

    MOMO

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,10 +1,15 @@
-
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-  const { priceId, sessionId, userEmail } = req.body;
+  const { priceId, userEmail } = req.body;
+
+  // Créez un nouveau client avec l'e-mail de l'utilisateur
+  const customer = await stripe.customers.create({
+    email: userEmail,
+  });
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -13,11 +18,12 @@ export default async function handler(req, res) {
         quantity: 1,
       },
     ],
+    // Associez le client à la session de paiement
+    customer: customer.id,
     mode: "payment",
     success_url: `${req.headers.origin}/dashboard`,
     cancel_url: `${req.headers.origin}/canceled`,
-    client_reference_id: userEmail,
-    customer_email: userEmail
   });
+
   res.status(200).json({ id: session.id });
 }
