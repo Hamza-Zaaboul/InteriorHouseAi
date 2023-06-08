50f1c394c2e705a83d94576f11481970e1c476ba
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-08 23:05:15 +0200

    rer

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
+const cors = require('cors')({
+    origin: ['http://localhost:3000', 'https://housemakere.vercel.app']
+  });
 
 const transporter = nodemailer.createTransport(
     smtpTransport({
@@ -10,30 +13,35 @@ const transporter = nodemailer.createTransport(
         port: 587,
         auth: {
             user: 'apikey',
-            pass: "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",
+            pass: "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",
 
         },
     })
 );
 
-exports.sendEmail = functions.https.onCall(async (data, context) => {
-    // Récupérez les données de l'e-mail à partir de la demande
-    const { email, subject, message } = data;
+exports.sendEmail = functions.https.onRequest((req, res) => {
+    // Autoriser toutes les requêtes CORS
+    cors(req, res, () => {
+        // Récupérez les données de l'e-mail à partir de la demande
+        const { email, subject, message } = req.body;
 
-    // Configuration du message
-    const mailOptions = {
-        from: email,
-        to: "zaaboul.freelance@gmail.com",
-        subject: subject,
-        text: message,
-    };
+        // Configuration du message
+        const mailOptions = {
+            from: email,
+            to: "zaaboul.freelance@gmail.com",
+            subject: subject,
+            text: message,
+        };
 
-    try {
         // Envoyer l'e-mail
-        await transporter.sendMail(mailOptions);
-        return { success: true };
-    } catch (error) {
-        console.error('Erreur lors de l\'envoi de l\'e-mail:', error);
-        return { success: false, error: error.message };
-    }
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Erreur lors de l\'envoi de l\'e-mail:', error);
+                res.status(500).json({ success: false, error: error.message });
+            } else {
+                console.log('E-mail envoyé:', info.response);
+                res.json({ success: true });
+            }
+        });
+    });
 });
