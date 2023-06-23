2335740966bd4fcda6179200376b31f6686aca5b
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-24 01:33:37 +0200

    Dernier ligne droite

--- components/dashboardui/utils/DownloadButtonHistorique.jsx
+++ components/dashboardui/utils/DownloadButtonHistorique.jsx
@@ -2,10 +2,11 @@ import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-
+import { useAuthContext } from "@/store/AuthContext";
 export default function DownloadButtonHistorique({ listselected }) {
   const [isDownloading, setIsDownloading] = useState(false);
   const [isDownloaded, setIsDownloaded] = useState(false);
+  const { user } = useAuthContext();
 
   const handleButtonClick = async () => {
     setIsDownloading(true);
@@ -16,7 +17,7 @@ export default function DownloadButtonHistorique({ listselected }) {
       const fetchPromises = listselected.map(async (url, index) => {
         try {
           const response = await fetch(
-            `/api/download?filePath=${encodeURIComponent(url)}`
+            `/api/download?filePath=${encodeURIComponent(url)}&uid=${user.uid}`
           );
           if (!response.ok) {
             throw new Error("Erreur lors du téléchargement du fichier");
