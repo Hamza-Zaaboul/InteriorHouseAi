07da7c24a02d5ae4a3afe9936d7e197972ac6cc5
Hamza ZAABOUL <122619493+ZelModall@users.noreply.github.com>
2023-06-24 22:27:16 +0200

    Site map

--- next.config.js
+++ next.config.js
@@ -1,26 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "replicate.com",
+        protocol: 'https',
+        hostname: 'replicate.com',
       },
       {
-        protocol: "https",
-        hostname: "replicate.delivery",
+        protocol: 'https',
+        hostname: 'replicate.delivery',
       },
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ];
   },
 };
 
